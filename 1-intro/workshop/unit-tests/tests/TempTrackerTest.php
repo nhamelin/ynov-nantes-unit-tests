6e8b5cbfdaae3e2569be7605e676e60d9c4eddea
nhamelin <n.hamelin44120@gmail.com>
2022-04-22 13:00:02 +0200

    intro workshop unit test

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -5,7 +5,32 @@ use PHPUnit\Framework\TestCase;
 require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
+    /*
+
     public function test_something() {
         $this->assertEquals(42, 42);
+    }
+
+    */
+
+    public function test_insert_int()
+    {
+        $tempTracker = new TempTracker();
+        $tempTracker->insert(9);
+        $this->assertEquals($tempTracker->get_temps(), [9]);
+    }
+
+    public function test_insert_string(){
+        $tracker = new TempTracker;
+
+        $this->expectException(\TypeError::class);
+        $tracker->insert("test");
+    }
+
+    public function test_insert_null(){
+        $tracker = new TempTracker;
+
+        $this->expectException(\TypeError::class);
+        $tracker->insert();
     }
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -6,8 +6,29 @@ require_once('./src/flatten.php');
 
 class FlattenTest extends TestCase
 {
+    /*
+    
     public function test_something()
     {
         $this->assertEquals(1337, 1337);
+    }
+
+    */
+
+    public function test_failure()
+    {
+        $this->expectError(PHPUnit\Framework\Error\Error::class);
+        flatten("test");
+    }
+
+    public function test_empty()
+    {
+        $this->assertEquals(flatten([null]), [null]);
+    }
+
+    public function test_3Level(){
+        $this->assertEquals(flatten([
+            [9, ["N", "J"]], ["Test1", "Test2"]]), 
+            [9, "N", "J", "Test1", "Test2"]);
     }
 }
