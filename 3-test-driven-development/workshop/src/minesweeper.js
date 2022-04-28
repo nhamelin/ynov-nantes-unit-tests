e639f0f3ac217e36db0e57566b50b2b26e6540f0
Descieux <descieuxnamdegone@gmail.com>
2022-04-28 13:49:11 +0200

    test feat default values

--- 3-test-driven-development/workshop/src/minesweeper.js
+++ 3-test-driven-development/workshop/src/minesweeper.js
@@ -4,9 +4,12 @@ class Square {
         if (m < 0 || m > 100 || n < 0 || n > 100) {
             this.m = 0;
             this.n = 0;
-        } else {
+        } else if (m != null || n != null) {
             this.m = m;
             this.n = n;
+        } else {
+            this.m = 4;
+            this.n = 4;
         }
     }
 }
--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -1,21 +1,38 @@
 const { Square } = require("../src/minesweeper");
 
+const inputBord = [
+    ["*", "*", ".", ".", "."],
+    [".", ".", ".", ".", "."],
+    [".", "*", ".", ".", "."]
+];
+const outputBord = [
+    ["*", "*", "1", "0", "0"],
+    ["3", "3", "2", "0", "0"],
+    ["1", "*", "1", "0", "0"]
+];
 
 describe("Minesweeper", function() {
     it("should create square", function() {
         const square = new Square(4, 4);
-        console.log(square.m, square.n);
         expect(square.m).toBe(4);
         expect(square.n).toBe(4);
 
 
     });
 
-    it("should line and column of square must betwen (0 < n,m <= 100)", function() {
-        const square = new Square(-2, 102);
-        console.log(square.m, square.n)
-        expect(square.m).toBe(0);
-        expect(square.n).toBe(0);
+    it
+        ("should line and column of square must betwen (0 < n,m <= 100)", function() {
+            const square = new Square(-2, 102);
+            expect(square.m).toBe(0);
+            expect(square.n).toBe(0);
+        });
+
+    it("should default values must be 4 4  ", function() {
+        const square = new Square();
+        expect(square.m).toBe(4);
+        expect(square.n).toBe(4);
     });
+
+
 
 });
