6c1669f4b314229f0e2166f939ae53d6f4d84d51
nhamelin <n.hamelin44120@gmail.com>
2022-04-28 14:16:58 +0200

    refacto

--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -22,15 +22,15 @@ describe("Minesweeper", function() {
     it("should line and column of square must betwen (0 < n,m <= 100)", function() {
             const board = new Minesweeper(-2, 102);
 
-            expect(board.m).toBe(0);
-            expect(board.n).toBe(0);
+            expect(board.x).toBe(0);
+            expect(board.y).toBe(0);
         });
 
     it("should default values must be 4 4  ", function() {
         const board = new Minesweeper();
 
-        expect(board.m).toBe(4);
-        expect(board.n).toBe(4);
+        expect(board.x).toBe(4);
+        expect(board.y).toBe(4);
     });
 
 });
