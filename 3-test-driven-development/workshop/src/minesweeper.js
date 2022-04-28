73a6cbc80245a49fc2e22ed3fb33424098d57da2
Descieux <descieuxnamdegone@gmail.com>
2022-04-28 15:07:47 +0200

    test feat get number of mines

--- 3-test-driven-development/workshop/src/minesweeper.js
+++ 3-test-driven-development/workshop/src/minesweeper.js
@@ -1,17 +1,59 @@
 class Minesweeper {
+    row = 0;
+    column = 0;
 
-    constructor(m, n) {
-        if (m < 0 || m > 100 || n < 0 || n > 100) {
-            this.m = 0;
-            this.n = 0;
-        } else if (m != null || n != null) {
-            this.m = m;
-            this.n = n;
+    constructor(x, y) {
+        if (x < 0 || y < 0) {
+            this.x = 0;
+            this.y = 0;
+        } else if (x > 100 || y > 100) {
+            this.x = 100;
+            this.y = 100;
+        } else if (x != null || y != null) {
+            this.x = x;
+            this.y = y;
         } else {
-            this.m = 4;
-            this.n = 4;
+            this.x = 4;
+            this.y = 4;
         }
     }
+
+    generateNewBoardAndgetNumberOfMines() {
+        let numberOfMines = 3;
+        let board = [];
+        this.row = 3;
+        this.column = 5;
+        for (let i = 0; i < this.row; i++) {
+            board[i] = [];
+            for (let j = 0; j < this.column; j++) {
+                board[i][j] = 0;
+            }
+        }
+
+        // Generate random mine placement
+        for (let i = 0; i < numberOfMines; i++) {
+            let hauteur = Math.floor(Math.random() * this.row);
+            let largeur = Math.floor(Math.random() * this.column);
+            // If no mine at indices, place mine
+            if (board[hauteur][largeur] === 0) {
+                board[hauteur][largeur] = "*";
+            } else {
+                i--;
+            }
+        }
+        //Generate proximity numbers
+        let nb = 0;
+        for (let i = 0; i < this.row; i++) {
+            for (let j = 0; j < this.column; j++) {
+                if (board[i][j] != 0) {
+                    nb++;
+                }
+            }
+        }
+
+        return nb;
+    }
+
 }
 module.exports = {
     Minesweeper
--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -1,36 +1,33 @@
 const { Minesweeper } = require("../src/minesweeper");
 
-const inputBord = [
-    ["*", "*", ".", ".", "."],
-    [".", ".", ".", ".", "."],
-    [".", "*", ".", ".", "."]
-];
-const outputBord = [
-    ["*", "*", "1", "0", "0"],
-    ["3", "3", "2", "0", "0"],
-    ["1", "*", "1", "0", "0"]
-];
-
 describe("Minesweeper", function() {
-    it("should create square", function() {
-        const square = new Minesweeper(4, 4);
+    it("should create Minesweeper", function() {
+        const board = new Minesweeper(15, 15);
 
-        expect(square.m).toBe(4);
-        expect(square.n).toBe(4);
+        expect(board.x).toBe(15);
+        expect(board.y).toBe(15);
     });
 
     it("should line and column of square must betwen (0 < n,m <= 100)", function() {
-            const square = new Square(-2, 102);
+        const board = new Minesweeper(-2, 102);
 
-            expect(square.m).toBe(0);
-            expect(square.n).toBe(0);
-        });
+        expect(board.x).toBe(0);
+        expect(board.y).toBe(0);
+    });
 
-    it("should default values must be 4 4  ", function() {
-        const square = new Square();
+    it("should return 4 4 as default value", function() {
+        const board = new Minesweeper();
 
-        expect(square.m).toBe(4);
-        expect(square.n).toBe(4);
+        expect(board.x).toBe(4);
+        expect(board.y).toBe(4);
+    });
+
+    it("should find nb mines in the bord  ", function() {
+        const bord = new Minesweeper();
+        let nbMines = bord.generateNewBoardAndgetNumberOfMines();
+        console.log(nbMines);
+
+        expect(nbMines).toBe(3);
     });
 
 });
