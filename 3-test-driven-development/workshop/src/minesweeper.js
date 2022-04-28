749f6d7be526b3df0ef786f6cacafb303bd5b414
Descieux <descieuxnamdegone@gmail.com>
2022-04-28 11:40:07 +0200

    feat line and column of square must betwen   (0 < n,m <= 100)

--- 3-test-driven-development/workshop/src/minesweeper.js
+++ 3-test-driven-development/workshop/src/minesweeper.js
@@ -1,8 +1,19 @@
 class Square {
 
     constructor(m, n) {
-        this.m = m;
-        this.n = n;
+        if (m < 0 || m > 100 || n < 0 || n > 100) {
+            this.m = 0;
+            this.n = 0;
+        } else if (m < 0 && m > 100 && n >= 0 && n <= 100) {
+            this.m = 0;
+            this.n = n;
+        } else if (n < 0 && n > 100 && m >= 0 && m <= 100) {
+            this.m = m;
+            this.n = 0
+        } else {
+            this.m = m;
+            this.n = n;
+        }
     }
 }
 module.exports = {
