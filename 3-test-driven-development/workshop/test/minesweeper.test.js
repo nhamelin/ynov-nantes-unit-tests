161f5e4a3aa70c108d05cb1912769cb25392b087
Descieux <descieuxnamdegone@gmail.com>
2022-04-28 11:46:25 +0200

    refacto  (0 < n,m <= 100)

--- 3-test-driven-development/workshop/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/test/minesweeper.test.js
@@ -4,18 +4,18 @@ const { Square } = require("../src/minesweeper");
 describe("Minesweeper", function() {
     it("should create square", function() {
         const square = new Square(4, 4);
+        console.log(square.m, square.n);
         expect(square.m).toBe(4);
         expect(square.n).toBe(4);
 
 
     });
 
-    it("should line and column of square must betwen   (0 < n,m <= 100)", function() {
+    it("should line and column of square must betwen (0 < n,m <= 100)", function() {
         const square = new Square(-2, 102);
+        console.log(square.m, square.n)
         expect(square.m).toBe(0);
         expect(square.n).toBe(0);
-
-
     });
 
 });
